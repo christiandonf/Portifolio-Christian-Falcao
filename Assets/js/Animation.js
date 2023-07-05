6630af27d75366434f5780000a0812178f3799d6
Ratix <christian.nascimento.falcao@gmail.com>
2023-07-05 20:00:35 -0300

    Novo Site

--- Assets/js/Animation.js
+++ Assets/js/Animation.js
@@ -1,1 +1,11 @@
-
+function toggleDiv() {
+  var div = document.getElementById("menu_bar_nav");
+  var header = document.getElementById("header_css");
+  if (div.style.display === "flex") {
+    div.style.display = "none";
+    header.style.borderRadius = "0 0 12px 12px"
+  } else {
+    div.style.display = "flex";
+    header.style.borderRadius = "0 0 0 0"
+  }
+}
